94d15a5b8d0e4e648430cbee755088e387b3f62f
Mastermind-code <80988577+Mastermind-code@users.noreply.github.com>
2022-06-24 22:42:20 -0700

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,9 @@
+const name = 'Adam'
+
+const height = "24m"
+
+const country = 'Nigeria'
+
+console.log('my name is ' + name)
+console.log('my height is ' + height)
+console.log('my country is ' + country)
